86485700246bf47510ddb7e08c395a6daec6471f
Lê Nhật Khanh <pankenzak@gmail.com>
2025-10-30 23:35:35 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #chỗ này để code tổng
-from display_manager import manager_display
 from display_user import user_display
 
 stop = False
@@ -9,10 +8,11 @@ while start:
   choose_display = int(input('1. Manager \n2. User \nPleaser enter your choice(1/2): '))
   
   if choose_display == 1:
-    manager_display()
+    user_display()
   elif choose_display == 2:
     user_display()
   else:  #Output sai
     continue_or_stop = input('\nWrong input\nIf you want to continue the program please enter "continue" to do\nIf not please enter "stop"\nYour choice: ')
     if continue_or_stop == 'stop':
       start = False
+#Thử commit bằng add file
