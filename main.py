42c81d7c2d82dff388cdf3624e6534d5a34e1d07
Lê Nhật Khanh <nhatkhanhle11906@gmail.com>
2025-10-30 22:38:53 +0700

    Refactor function calls for manager and user displays
    fix bug

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ while start:
   choose_display = int(input('1. Manager \n2. User \nPleaser enter your choice(1/2): '))
   
   if choose_display == 1:
-    manager_display()
+    manager_display.manager_display()
   elif choose_display == 2:
-    user_display()
+    user_display.user_display()
   else:  #Output sai
     continue_or_stop = input('\nWrong input\nIf you want to continue the program please enter "continue" to do\nIf not please enter "stop"\nYour choice: ')
     if continue_or_stop == 'stop':
