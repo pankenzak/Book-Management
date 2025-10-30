1e878bfc49e20d9a800efd90e788dede6d4ab17b
Lê Nhật Khanh <nhatkhanhle11906@gmail.com>
2025-10-30 15:14:03 +0700

    Add ManagerDisplay class for book management
    Tự động khai báo sách

--- display_manager.py
+++ display_manager.py
@@ -1,1 +1,11 @@
 #code giao diện quản lý
+class ManagerDisplay:
+  def __init__ (self, name_book, author, category, producer):
+    self.name_book = name_book
+    self.author = author
+    self.category = category
+    self.producer = producer
+
+  def add_book(self, name_book, author, category, producer):
+    
+  
