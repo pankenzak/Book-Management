eb85d8fc01f72e3e3cd2fe2c1bf78157eb1e9096
Lê Nhật Khanh <nhatkhanhle11906@gmail.com>
2025-10-30 22:29:20 +0700

    Fix comparison operators in user_display function
    Sữa lỗi gán ở while

--- display_user.py
+++ display_user.py
@@ -8,13 +8,13 @@ def show_menu():
   
 def user_display():
   back = False
-  while back = False:  #while nay dung de back cua phan lua chon lam gi
+  while back == False:  #while nay dung de back cua phan lua chon lam gi
     to_do = int(input('\n1. Search\n2. Terms\n3. News\n4. Back\nPlease enter your choice(1/2): '))
     
     if to_do == 1:
       print("FILTER")
       back_1 = False
-      while back_1 = False:  #while nay dung de back cua phan Filter
+      while back_1 == False:  #while nay dung de back cua phan Filter
         to_do_1 = int(input('\n1. All\n2. Author\n3. Category\n4. Back\nPlease enter your choice(1/2/3/4): ')) #Nhung input khac doi build data sau do lien ket
         if to_do_1 == 4:  #back ve chon lam gi
           back_1 = True
