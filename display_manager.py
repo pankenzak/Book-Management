da0e2d240f74aa148f71329e73a8d1340845b510
Lê Nhật Khanh <pankenzak@gmail.com>
2025-10-31 18:07:05 +0700

    Build add book
    đã xong add book

--- display_manager.py
+++ display_manager.py
@@ -2,8 +2,8 @@
 from data_book import DataBook
 
 def use_data_book():
-    with open('FileBook.txt', 'r') as file: #Doc File sach
-        book_list = file.readlines()
+    with open('FileBook.txt', 'r') as file_r: #Doc file sach
+        book_list = file_r.readlines()
         
         def print_all_book():
             for i in book_list:
@@ -11,8 +11,9 @@ def use_data_book():
                 import_data = DataBook(book_materies[0], book_materies[1], book_materies[2], book_materies[3], book_materies[4])
                 print(f'\n{import_data.print_all_book()}') #Khai bao sach vao va in ra
                 
-        def add_book():
-            print('dang buil add')
+        def add_book(name_book, author, category, producer, quantity):
+            with open('FileBook.txt', 'w') as file_w: #Mo lai file sach o che do ghi
+                file_w.writelines(f'{name_book}; {author}; {category}; {producer}; {quantity}')
             pass
         
         def update_book():
@@ -28,12 +29,21 @@ def use_data_book():
             to_do_1 = int(input('\n1. View all book list \n2. Add book\n3. Update book\n4. Delete book\n5. Back\nPleaser enter your choice(1/2/3/4): '))
             if to_do_1 == 1:
                 print_all_book()
+                
             elif to_do_1 == 2:
-                add_book()
+                name_book = input('\nEnter book name: ')
+                author = input('Enter author name: ')
+                category = input('Enter category: ')
+                producer = input('Enter producer: ')
+                quantity = int(input('Enter quantity: '))
+                add_book(name_book, author, category, producer, quantity)
+                
             elif to_do_1 == 3:
                 update_book()
+                
             elif to_do_1 == 4:
                 delete_book()
+                
             elif to_do_1 == 5 :
                 back = True
             else:
