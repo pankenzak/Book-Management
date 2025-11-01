45f664121f770c13639436f88962c7c4de890c8d
Lê Nhật Khanh <nhatkhanhle11906@gmail.com>
2025-11-02 00:16:53 +0700

    thêm 2 thuộc tính cho data book
    Thêm năm xuất bản và có khả năng mượn sách hay không

--- data_book.py
+++ data_book.py
@@ -1,11 +1,14 @@
 #Chỗ này để code data của sách
 class DataBook:
-    def __init__(self, id_book, name_book, author, category, producer, quantity):
+    def __init__(self, id_book:str, name_book:str, author:str, category:str, publication_year:int, producer: str, quantity:int, available:bool):
         self.id_book = id_book
         self.name_book = name_book
         self.author = author
         self.category = category
+        self.publication_year = publication_year
         self.producer = producer
         self.quantity = quantity
+        self.available = available
         
+
     
